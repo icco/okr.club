b3e4e7fcf3cff1581a5d770e5ba1af1443dee462
Nat Welch <nat@natwelch.com>
2015-07-30 21:36:06 +0000

    add objectives

---
+++ db/migrate/20150730211139_create_objectives.rb
@@ -0,0 +1,12 @@
+class CreateObjectives < ActiveRecord::Migration
+  def change
+    create_table :objectives do |t|
+      t.text :text
+      t.date :start
+      t.date :end
+      t.integer :user_id
+
+      t.timestamps, null: false
+    end
+  end
+end
