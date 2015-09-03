1d2d4abbc5d3f6c21ca9ff8e448b6cb00379c926
Nat Welch <nat@natwelch.com>
2015-09-03 18:26:30 +0000

    close flash

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) {
+  var btn = document.getElementById('removeFlash');
+  if (btn.addEventListener) {
+    // DOM2 standard
+    btn.addEventListener('click', function() {
+      btn.parentNode.remove();
+      return false;
+    }, false);
+  }
 });
